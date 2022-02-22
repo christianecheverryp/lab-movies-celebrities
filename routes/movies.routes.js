611f99d0d948e4887ab4c5e8cda657213182c0df
Eva G Cagigas <evarteca@gmail.com>
2022-02-22 18:16:22 +0100

    all iterations + some styles

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,9 +1,10 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+
 const router = require("express").Router();
 const MovieModel = require("../models/Movie.model")
 const Celebrity = require("../models/Celebrity.model")
 
-// all your routes here
+// RUTA PARA AÑADIR PELÍCULAS (ITERACIÓN 3):
+
 router.get("/create", (req, res, next) => {
     Celebrity.find()
     .then((allCelebrities) => {
@@ -30,11 +31,71 @@ router.post("/create", (req, res, next) => {
     })
 })
 
+// RUTA PARA VER EL LISTADO DE PELÍCULAS (ITERACIÓN 7):
 
+router.get("/", (req, res, next) => {
+    MovieModel.find()
+    .then((allMovies) => {
+        res.render("movies/movies.hbs", {allMovies})
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
 
+// RUTA PARA VER LOS DETALLES DE CADA PELÍCULA (ITERACIÓN 8):
 
+router.get("/:id", (req, res, next)=>{
+    const {id} = req.params
+    MovieModel.findById(id)
+    .populate("cast")
+    .then((oneMovie)=>{
+        res.render("movies/movie-details.hbs", {oneMovie})
+    })
+    .catch((err) => {
+        next(err)
+    })
 
+})
 
+// RUTA PARA BORRAR UNA PELÍCULA (ITERACIÓN 9):
+
+router.post("/:id/delete", (req, res, next)=>{
+    const {id} = req.params
+    MovieModel.findByIdAndDelete(id)
+    .then((deleteMovie)=>{
+        res.redirect("/movies")
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+// RUTA PARA EDITAR UNA PELÍCULA (ITERACIÓN 10):
+
+router.get("/:id/edit", async (req, res, next)=>{
+    try{
+        const {id} = req.params;
+        const oneMovie = await MovieModel.findById(id)
+        const allCast = await Celebrity.find()
+        res.render("movies/edit-movie.hbs", {oneMovie, allCast})
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.post("/:id/edit", async (req, res, next)=>{
+    try{
+        const {id} = req.params;
+        const {title, genre, plot, cast} = req.body
+        const update = await MovieModel.findByIdAndUpdate(id, {title, genre, plot, cast})
+        res.redirect("/movies")
+    }
+    catch(err){
+        next(err)
+    }
+})
 
 
 module.exports = router;
